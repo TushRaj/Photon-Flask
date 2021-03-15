d2807b097432af46931c87abef931445b1c116c8
TushRaj <tushraj21@gmail.com>
2021-03-15 21:38:52 +0530

    sqlite addition

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///photon.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        photon = Photon(title=title, desc=desc)
+        db.session.add(photon)
+        db.session.commit()
+        
+    all = Photon.query.all() 
+    return render_template('index.html', all=all)
 
-@app.route('/products')
-def products():
-    return 'This is products page'
 
 if __name__ == "__main__":
     app.run(debug=True)
