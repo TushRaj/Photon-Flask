26598a8f80e2b5c97c95a28eac4db1cc1b0daee9
TushRaj <tushraj21@gmail.com>
2021-03-15 17:08:36 +0530

    sqlite db

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Photon(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+
+
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    #return 'Hello, World!'
 
 @app.route('/products')
 def products():
